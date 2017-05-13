74b380a0f94194ad5cdc83a2e3604ea82aaac290
Yaroslav Zakharchuk <yzakharc@e1r9p15.unit.ua>
2017-05-13 22:32:19 +0300

    Good

--- print_wchar.c
+++ print_wchar.c
@@ -6,7 +6,7 @@
 /*   By: yzakharc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 16:38:53 by yzakharc          #+#    #+#             */
-/*   Updated: 2017/05/12 21:59:56 by yzakharc         ###   ########.fr       */
+/*   Updated: 2017/05/13 22:31:46 by yzakharc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,41 +66,48 @@ void	print_three_bytes(int tmp, int *i, t_pf *pf)
 	pf->str = add_to_string(pf->str, tmp);
 }
 
-void	print_two_bytes(int tmp, int *i, t_pf *pf)
+void	print_two_bytes(int tmp, int *i, t_pf *pf, int *printed_width)
 {
-	tmp = *i >> 6;
-	tmp |= 192;
-	pf->str = add_to_string(pf->str, tmp);
-	tmp = *i;
-	tmp &= 63;
-	tmp |= 128;
-	pf->str = add_to_string(pf->str, tmp);
+	if (*i < 256 && (*printed_width)++ < pf->dot)
+	{
+		tmp = *i;
+		pf->str = add_to_string(pf->str, tmp);
+	}
+	if ((*i >= 256 && *i < 2048) && (*printed_width += 2) <= pf->dot)
+	{
+		tmp = *i >> 6;
+		tmp |= 192;
+		pf->str = add_to_string(pf->str, tmp);
+		tmp = *i;
+		tmp &= 63;
+		tmp |= 128;
+		pf->str = add_to_string(pf->str, tmp);
+	}
 }
 
 void	print_wchar(va_list *fm, t_pf *pf)
 {
 	int	*i;
 	int	tmp;
-	int printed_width;
+	int	printed_width;
 
+	if (!(i = va_arg(*fm, int *)))
+	{
+		pf->str = "(null)";
+		return ;
+	}
 	tmp = 0;
 	printed_width = 0;
 	pf->str = ft_strnew(1, pf);
-	i = va_arg(*fm, int *);
 	pf->dot == 0 ? pf->dot = 2097152 : 0;
 	while (*i)
 	{
-		if (*i < 128 && printed_width++ <= pf->dot)
-		{
-			tmp = *i;
-			pf->str = add_to_string(pf->str, tmp);
-		}
-		if ((*i >= 128 && *i < 2048) && (printed_width += 2) <= pf->dot)
-			print_two_bytes(tmp, i, pf);
+		print_two_bytes(tmp, i, pf, &printed_width);
 		if ((*i >= 2048 && *i < 65536) && (printed_width += 3) <= pf->dot)
 			print_three_bytes(tmp, i, pf);
 		if ((*i >= 65546 && *i < 2097152) && (printed_width += 4) <= pf->dot)
 			print_four_bytes(tmp, i, pf);
 		i++;
 	}
+	pf->dot = 0;
 }
